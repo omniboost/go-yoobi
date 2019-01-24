13d0ce4c467a04cd9c3af98a691cc5e90f005f50
Leon Bogaert <leon@tim-online.nl>
2019-01-24 17:37:32 +0100

    Added employee posts endpoint

---
+++ employee_post.go
@@ -0,0 +1,137 @@
+package yoobi
+
+import (
+	"net/http"
+	"net/url"
+)
+
+func (c *Client) NewEmployeesPostRequest() EmployeesPostRequest {
+	return EmployeesPostRequest{
+		client:      c,
+		queryParams: c.NewEmployeesPostQueryParams(),
+		pathParams:  c.NewEmployeesPostPathParams(),
+		method:      http.MethodPost,
+		headers:     http.Header{},
+		requestBody: c.NewEmployeesPostRequestBody(),
+	}
+}
+
+type EmployeesPostRequest struct {
+	client      *Client
+	queryParams *EmployeesPostQueryParams
+	pathParams  *EmployeesPostPathParams
+	method      string
+	headers     http.Header
+	requestBody EmployeesPostRequestBody
+}
+
+func (c *Client) NewEmployeesPostQueryParams() *EmployeesPostQueryParams {
+	return &EmployeesPostQueryParams{}
+}
+
+type EmployeesPostQueryParams struct{}
+
+func (p EmployeesPostQueryParams) ToURLValues() (url.Values, error) {
+	encoder := newSchemaEncoder()
+	params := url.Values{}
+
+	err := encoder.Encode(p, params)
+	if err != nil {
+		return params, err
+	}
+
+	return params, nil
+}
+
+func (r *EmployeesPostRequest) QueryParams() *EmployeesPostQueryParams {
+	return r.queryParams
+}
+
+func (c *Client) NewEmployeesPostPathParams() *EmployeesPostPathParams {
+	return &EmployeesPostPathParams{}
+}
+
+type EmployeesPostPathParams struct {
+}
+
+func (p *EmployeesPostPathParams) Params() map[string]string {
+	return map[string]string{}
+}
+
+func (r *EmployeesPostRequest) PathParams() *EmployeesPostPathParams {
+	return r.pathParams
+}
+
+func (r *EmployeesPostRequest) SetMethod(method string) {
+	r.method = method
+}
+
+func (r *EmployeesPostRequest) Method() string {
+	return r.method
+}
+
+func (s *Client) NewEmployeesPostRequestBody() EmployeesPostRequestBody {
+	return EmployeesPostRequestBody{}
+}
+
+type EmployeesPostRequestBody struct {
+	EmployeeNumber        string       `json:"employeenumber"`
+	BSN                   string       `json:"bsn"`
+	DateOfBirth           Date         `json:"dateofbirth"`
+	FirstName             string       `json:"firstname"`
+	Initials              string       `json:"initials"`
+	Infix                 string       `json:"infix"`
+	LastName              string       `json:"lastname"`
+	Gender                string       `json:"gender"`
+	EmployeeAbbr          string       `json:"employeeabbr"`
+	JobName               string       `json:"jobname"`
+	StartRegistrationDate Date         `json:"startregistrationdate"`
+	StartDate             Date         `json:"startdate"`
+	EndDate               Date         `json:"enddate"`
+	CostCenter            string       `json:"costcenter"`
+	CostUnit              string       `json:"costunit"`
+	AllowOvertime         Bool         `json:"allowovertime"`
+	Department            Department   `json:"department"`
+	State                 string       `json:"state"`
+	User                  User         `json:"user"`
+	CustomFields          CustomFields `json:"customfields"`
+}
+
+func (r *EmployeesPostRequest) RequestBody() *EmployeesPostRequestBody {
+	return &r.requestBody
+}
+
+func (r *EmployeesPostRequest) SetRequestBody(body EmployeesPostRequestBody) {
+	r.requestBody = body
+}
+
+func (r *EmployeesPostRequest) NewResponseBody() *EmployeesPostResponseBody {
+	return &EmployeesPostResponseBody{}
+}
+
+type EmployeesPostResponseBody struct {
+	Metadata Metadata  `json:"metadata"`
+	Results  Employees `json:"results"`
+}
+
+func (r *EmployeesPostRequest) URL() url.URL {
+	return r.client.GetEndpointURL("employees", r.PathParams())
+}
+
+func (r *EmployeesPostRequest) Do() (EmployeesPostResponseBody, error) {
+	// Create http request
+	req, err := r.client.NewRequest(nil, r.Method(), r.URL(), r.RequestBody())
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	// Process query parameters
+	err = AddQueryParamsToRequest(r.QueryParams(), req, false)
+	if err != nil {
+		return *r.NewResponseBody(), err
+	}
+
+	responseBody := r.NewResponseBody()
+	_, err = r.client.Do(req, responseBody)
+	return *responseBody, err
+}
--- types.go
+++ types.go
@@ -1,15 +1,79 @@
 package yoobi
 
-import uuid "github.com/satori/go.uuid"
+import (
+	"encoding/json"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type Employees []Employee
 
 type Employee struct {
 	Lastname       string    `json:"lastname"`
-	Employeeid     uuid.UUID `json:"employeeid"`
-	Employeenumber string    `json:"employeenumber"`
+	EmployeeID     uuid.UUID `json:"employeeid"`
+	EmployeeNumber string    `json:"employeenumber"`
 	Self           URL       `json:"self"`
 	State          string    `json:"state"`
-	Firstname      string    `json:"firstname"`
+	FirstName      string    `json:"firstname"`
 	Infix          string    `json:"infix"`
+}
+
+type Bool bool
+
+func (b Bool) MarshalJSON() ([]byte, error) {
+	if b == true {
+		return json.Marshal("1")
+	}
+	return json.Marshal("0")
+}
+
+type Department struct {
+	Name string `json:"name"`
+}
+
+type User struct {
+	State string `json:"state"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type CustomFields []CustomField
+
+type CustomField struct{}
+
+type Date struct {
+	time.Time
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.Format("02-01-2006"))
+}
+
+func (d *Date) UnmarshalJSON(data []byte) (err error) {
+	var value string
+	err = json.Unmarshal(data, &value)
+	if err != nil {
+		return err
+	}
+
+	if value == "" {
+		return nil
+	}
+
+	// first try standard date
+	d.Time, err = time.Parse(time.RFC3339, value)
+	if err == nil {
+		return nil
+	}
+
+	// try iso8601 date format
+	d.Time, err = time.Parse("2006-01-02", value)
+	if err == nil {
+		return nil
+	}
+
+	// try yoobi date format
+	d.Time, err = time.Parse("02-01-2006", value)
+	return err
 }
